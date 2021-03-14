1695d14a65a7d83d3affec7e33a5d954df24ae41
juliagarlor <julia.garcia.lorente@gmail.com>
2021-03-14 23:03:13 +0100

    screenshots up

--- Backend/eureka-service/src/main/resources/static/pettureDBAndTables.sql
+++ Backend/eureka-service/src/main/resources/static/pettureDBAndTables.sql
@@ -27,19 +27,6 @@ FOREIGN KEY(requested_user) REFERENCES `user`(user_name),
 FOREIGN KEY(requesting_user) REFERENCES `user`(user_name)
 );
 
--- INSERT INTO `user` (user_name, `password`, profile_picture, visibility, `role`) VALUES 
--- -- Contraseña perry: malditaSeaPerryElOrnitorrinco
--- ('perryThePlatypus', '$2a$10$D4RylHFq71wpbaufSeL.2uULHmpz.Gtf6yO5kFnC7h8h6kv6zw5U6', 'assets/images/perry.jpg', 'PUBLIC', 'USER'),
--- -- contraseña garfield: lasaña
--- ('garfieldLovesMondays', '$2a$10$cFlNgE2vRoTuDAc3/rjEI.uizoiKOY/3r2Xq6MoRinOZ5WWoYD5NS', 'assets/images/garfield.jpg', 'PUBLIC', 'USER');
-
--- INSERT INTO request_to_user(requested_user, requesting_user) VALUES
--- ('perryThePlatypus', 'garfieldLovesMondays');
-
--- INSERT INTO user_buddies(`current_user`, buddy) VALUES
--- ('perryThePlatypus', 'garfieldLovesMondays'), 
--- ('garfieldLovesMondays','perryThePlatypus');
-
 -- Picture database. Tables: picture
 
 DROP SCHEMA IF EXISTS picture;
@@ -53,10 +40,6 @@ picture_name VARCHAR(255),
 pic BLOB,
 PRIMARY KEY(id)
 );
-
--- INSERT INTO picture(picture_name, user_name, licks) VALUES
--- ('assets/images/perry.jpg', 'perryThePlatypus', 10),
--- ('assets/images/garfield.jpg', 'garfieldLovesMondays', 12);
 
 -- Post database. Tables: post and commentary
 
@@ -81,14 +64,3 @@ post_id BIGINT,
 PRIMARY KEY(id),
 FOREIGN KEY(post_id) REFERENCES post(id)
 );
-
--- INSERT INTO post(body, picture_id) VALUES
--- ('Fighting evil in my Vespa', 1),
--- ('It\'s Friday, guys!', 2);
-
--- INSERT INTO commentary(user_name, body, post_id) VALUES 
--- ('garfieldLovesMondays', 'Who', 1),
--- ('garfieldLovesMondays', 'the hell', 1),
--- ('garfieldLovesMondays', 'is driving that thing?', 1),
--- ('perryThePlatypus', 'Chubby cat', 2)
--- ;
